9c5488192ab6362fa2ad26f8e3e0cded021712d5
anhbh <jangkyavn@gmail.com>
2019-12-18 17:28:54 +0700

    test sign bill

--- MyWorkerService/Worker.cs
+++ MyWorkerService/Worker.cs
@@ -1,10 +1,15 @@
-using Microsoft.AspNetCore.SignalR.Client;
+/// https://tuhocict.com/thuc-thi-truy-van-sql-trong-c-lop-sqlcommand/
+
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using Spire.Pdf;
+using Spire.Pdf.General.Find;
+using Spire.Pdf.Graphics;
+using Spire.Pdf.Security;
 using System;
-using System.Net.Http;
-using System.Net.Http.Headers;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -13,8 +18,7 @@ namespace MyWorkerService
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
-        private HubConnection connection;
-        private string _accessToken;
+        private string _connectionString = "Server=117.7.227.159;Database=ElectronicBill;User Id=sa;Password=Bach@khoa;MultipleActiveResultSets=true";
 
         public Worker(ILogger<Worker> logger)
         {
@@ -25,47 +29,7 @@ namespace MyWorkerService
         {
             _logger.LogInformation("Service Started at: {time}", DateTimeOffset.Now);
 
-            RunAsync().Wait();
-
-            connection = new HubConnectionBuilder()
-                .WithUrl("https://localhost:44383/notify", options =>
-                {
-                    options.AccessTokenProvider = () => Task.FromResult(_accessToken);
-                })
-                .Build();
-
             return base.StartAsync(cancellationToken);
-        }
-
-        private async Task RunAsync()
-        {
-            using (var client = new HttpClient())
-            {
-                client.BaseAddress = new Uri("https://localhost:44383/");
-                client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-                HttpResponseMessage response = await client.PostAsJsonAsync("api/Auth/login", new
-                {
-                    userName = "admin",
-                    password = "123456"
-                });
-
-                if (response.IsSuccessStatusCode)
-                {
-                    LoginReponse loginReponse = await response.Content.ReadAsAsync<LoginReponse>();
-                    _accessToken = loginReponse.Token;
-
-                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _accessToken);
-                    HttpResponseMessage productResponse = await client.GetAsync("api/Product");
-                    if (productResponse.IsSuccessStatusCode)
-                    {
-                        Product[] products = await productResponse.Content.ReadAsAsync<Product[]>();
-
-                        _logger.LogInformation("products: {0}", JsonConvert.SerializeObject(products));
-                    }
-                }
-            }
         }
 
         public override Task StopAsync(CancellationToken cancellationToken)
@@ -78,42 +42,99 @@ namespace MyWorkerService
         {
             //while (!stoppingToken.IsCancellationRequested)
             //{
-            //    //if (GetResponse())
-            //    //{
-            //    //    _logger.LogInformation("Response is OK: {time}", DateTimeOffset.Now);
-            //    //}
-            //    //else
-            //    //{
-            //    //    _logger.LogInformation("No Response: {time}", DateTimeOffset.Now);
-            //    //}    
 
-            //    await Task.Delay(0, stoppingToken);
+
+            //    await Task.Delay(1000, stoppingToken);
             //}
 
-            connection.On<string>("ReceiveMessage1", (message) =>
-            {
-                _logger.LogInformation("Message: {0}", message);
-            });
 
-            await connection.StartAsync();
+            QueryData();
         }
 
-        public bool GetResponse()
+        private void QueryData()
         {
-            return true;
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                var command = new SqlCommand("SELECT * FROM Bills where ApproveStatus = 7", connection);
+                var sqlDataReader = command.ExecuteReader();
+                if (sqlDataReader.HasRows)
+                {
+                    while (sqlDataReader.Read())
+                    {
+                        var id = new Guid(sqlDataReader["Id"].ToString());
+                        var unsignFileByte = sqlDataReader["UnsignFileByte"] as byte[];
+
+                        System.IO.File.WriteAllBytes("abc.pdf", unsignFileByte);
+
+                        // Load a PDF file and certificate
+                        PdfDocument pdf = new PdfDocument();
+                        pdf.LoadFromFile("abc.pdf");
+                        int end = pdf.Pages.Count - 1;
+                        PdfPageBase page = pdf.Pages[end];
+
+                        PdfCertificate cert = new PdfCertificate("dtbk.pfx", "DienTuBachKhoa");
+
+                        // Create a signature and set its position.
+                        PdfSignature signature = new PdfSignature(pdf, page, cert, "Dinh Vi Bach Khoa");
+                        signature.SignDetailsFont = new PdfTrueTypeFont(new Font("Times New Roman", 8f, FontStyle.Bold), true);
+                        signature.SignFontColor = Color.Red;
+
+                        signature.Bounds = new RectangleF(GetPositionSign(page), new SizeF(212, 40));
+                        // Set the property for displaying the signature.
+                        // Fill the contents of the signature.
+                        signature.NameLabel = "Signature Valid\nKý bởi: ";
+                        signature.Name = string.Format("CT CP THIẾT BỊ ĐIỆN - ĐIỆN TỬ BÁCH KHOA\nNgày ký: {0}\n\n\n", DateTime.Now.ToString("dd/MM/yyyy"));
+                        signature.DistinguishedName = "ĐỊNH VỊ BÁCH KHOA";
+
+                        signature.ReasonLabel = "\nReason: ";
+                        signature.Reason = "Hóa đơn giá trị gia tăng";                                                      // Add
+
+                        signature.DateLabel = "\nNgày ký: ";
+                        signature.Date = DateTime.Now;
+
+                        signature.ContactInfoLabel = "Tổng đài CSKH: ";
+                        signature.ContactInfo = "1900 5555 13";                                                             // Add
+                        signature.LocationInfo = "Số 561 Nguyễn Bỉnh Khiêm, Hải An, Hải Phòng, Việt Nam";                   // Aad
+
+                        signature.Certificated = false;
+
+                        // Set the document permission of the signature.
+                        signature.DocumentPermissions = PdfCertificationFlags.ForbidChanges;
+
+                        //Save pdf file.
+                        string signedFileName = "signed" + ".pdf";
+                        pdf.SaveToFile(signedFileName);
+                        pdf.Close();
+
+                        var updateQuery = "UPDATE [dbo].[Bills] SET [SignedFileByte] = @SignedFileByte WHERE [Id] = @Id";
+                        //command.CommandText = updateQuery;
+                        //command.Parameters.AddWithValue("SignedFileByte", lastName);
+                        //count = command.ExecuteNonQuery();
+                        _logger.LogInformation("Update Bill has Id = {0}", id.ToString());
+                    }
+                }
+
+                sqlDataReader.Close();
+            }
         }
-    }
 
-    public class Product
-    {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-    }
+        private PointF GetPositionSign(PdfPageBase page)
+        {
+            // Create default
+            PointF point = new PointF(335, 685);
 
-    public class LoginReponse
-    {
-        public string Token { get; set; }
-        public bool Status { get; set; }
-        public string Message { get; set; }
+            PdfTextFind[] result = page.FindText("Người bán hàng (Seller)").Finds;
+
+            if (result != null && result.Count() > 0)
+            {
+                point = result[0].Position;
+
+                point.X -= 35;
+                point.Y += 20;
+            }
+
+            return point;
+        }
     }
 }
