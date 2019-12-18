584258acebc0febe994c1f0278eb4bcf8e2dfc3b
anhbh <jangkyavn@gmail.com>
2019-12-19 06:15:11 +0700

    test sign bill successfully

--- MyWorkerService/Worker.cs
+++ MyWorkerService/Worker.cs
@@ -9,7 +9,10 @@ using Spire.Pdf.Security;
 using System;
 using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -40,36 +43,33 @@ namespace MyWorkerService
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            //while (!stoppingToken.IsCancellationRequested)
-            //{
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await QueryDataAsync();
 
-
-            //    await Task.Delay(1000, stoppingToken);
-            //}
-
-
-            QueryData();
+                await Task.Delay(1000, stoppingToken);
+            }
         }
 
-        private void QueryData()
+        private async Task QueryDataAsync()
         {
             using (var connection = new SqlConnection(_connectionString))
             {
-                connection.Open();
-                var command = new SqlCommand("SELECT * FROM Bills where ApproveStatus = 7", connection);
-                var sqlDataReader = command.ExecuteReader();
+                await connection.OpenAsync();
+                SqlCommand command = new SqlCommand("SELECT * FROM Bills where ApproveStatus = 7", connection);
+                SqlDataReader sqlDataReader = await command.ExecuteReaderAsync();
                 if (sqlDataReader.HasRows)
                 {
-                    while (sqlDataReader.Read())
+                    if (sqlDataReader.Read())
                     {
-                        var id = new Guid(sqlDataReader["Id"].ToString());
-                        var unsignFileByte = sqlDataReader["UnsignFileByte"] as byte[];
+                        Guid id = new Guid(sqlDataReader["Id"].ToString());
+                        byte[] unsignFileByte = sqlDataReader["UnsignFileByte"] as byte[];
 
-                        System.IO.File.WriteAllBytes("abc.pdf", unsignFileByte);
+                        await File.WriteAllBytesAsync("unsign.pdf", unsignFileByte);
 
                         // Load a PDF file and certificate
                         PdfDocument pdf = new PdfDocument();
-                        pdf.LoadFromFile("abc.pdf");
+                        pdf.LoadFromFile("unsign.pdf");
                         int end = pdf.Pages.Count - 1;
                         PdfPageBase page = pdf.Pages[end];
 
@@ -81,8 +81,6 @@ namespace MyWorkerService
                         signature.SignFontColor = Color.Red;
 
                         signature.Bounds = new RectangleF(GetPositionSign(page), new SizeF(212, 40));
-                        // Set the property for displaying the signature.
-                        // Fill the contents of the signature.
                         signature.NameLabel = "Signature Valid\nKý bởi: ";
                         signature.Name = string.Format("CT CP THIẾT BỊ ĐIỆN - ĐIỆN TỬ BÁCH KHOA\nNgày ký: {0}\n\n\n", DateTime.Now.ToString("dd/MM/yyyy"));
                         signature.DistinguishedName = "ĐỊNH VỊ BÁCH KHOA";
@@ -99,18 +97,36 @@ namespace MyWorkerService
 
                         signature.Certificated = false;
 
-                        // Set the document permission of the signature.
                         signature.DocumentPermissions = PdfCertificationFlags.ForbidChanges;
 
-                        //Save pdf file.
                         string signedFileName = "signed" + ".pdf";
                         pdf.SaveToFile(signedFileName);
                         pdf.Close();
 
-                        var updateQuery = "UPDATE [dbo].[Bills] SET [SignedFileByte] = @SignedFileByte WHERE [Id] = @Id";
-                        //command.CommandText = updateQuery;
-                        //command.Parameters.AddWithValue("SignedFileByte", lastName);
-                        //count = command.ExecuteNonQuery();
+                        SqlCommand updateBillCommand = new SqlCommand("UPDATE [dbo].[Bills] SET [SignedFileByte] = @SignedFileByte WHERE [Id] = @Id", connection);
+                        updateBillCommand.Parameters.AddWithValue("Id", id);
+                        updateBillCommand.Parameters.AddWithValue("SignedFileByte", File.ReadAllBytes(signedFileName));
+                        await updateBillCommand.ExecuteNonQueryAsync();
+
+                        using (var client = new HttpClient())
+                        {
+                            client.BaseAddress = new Uri("https://localhost:44383/");
+                            client.DefaultRequestHeaders.Accept.Clear();
+                            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                            try
+                            {
+                                await client.PostAsJsonAsync("api/Bill/postSignedBill", new
+                                {
+                                    Id = id
+                                });
+                            }
+                            catch (Exception ex)
+                            {
+                                _logger.LogError("Error Post Data: {0}", ex);
+                            }
+                        }
+
                         _logger.LogInformation("Update Bill has Id = {0}", id.ToString());
                     }
                 }
